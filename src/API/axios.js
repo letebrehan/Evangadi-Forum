f28c49af83fac00429d47c14a256fee512893d7d
Dereje Masresha <deremas27@gmail.com>
2025-04-04 01:08:49 +0300

    deploy

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3456/api",
+  baseURL: "https://evangadi-forum-starter-pw48.onrender.com/api",
+  // baseURL: "http://localhost:3456/api",
 });
 
 export default axiosInstance;
