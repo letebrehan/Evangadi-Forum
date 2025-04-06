a807bddfbad079677cec22ad3afdbe1a69ded216
letebrehan <henockmel@gmail.com>
2025-04-06 16:32:17 -0600

    api updated

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://evangadi-forum-starter-pw48.onrender.com/api",
+  baseURL: "https://evangadi-forum-starter-raqr.onrender.com/api",
   // baseURL: "http://localhost:3456/api",
 });
 
